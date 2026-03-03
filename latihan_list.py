226f36944d317004bb15ffb373ed5d9c43a82445
Ferian Satria <148834456+ferian-satria@users.noreply.github.com>
2026-03-03 10:41:00 +0700

    Create latihan_list.py

---
+++ latihan_list.py
@@ -0,0 +1,18 @@
+# NAMA  : 
+# KELAS : 
+# ---------------------------------------------------------
+# LATIHAN: REVIEW LIST PYTHON
+#Diberikan sebuah data acak nilai ujian siswa. Buatlah program yang mengurutkan data tersebut 
+#dari nilai tertinggi ke terendah. Kemudian, pisahkan 3 nilai tertinggi sebagai penerima beasiswa, 
+#dan hapus nilai terendah (nilai di bawah 60) karena dianggap tidak lulus. 
+#Gunakan data awal berikut: nilai_ujian = [75, 55, 90, 85, 45, 95, 80] 
+# ---------------------------------------------------------
+
+# OUTPUT
+#Data nilai asli: [75, 55, 90, 85, 45, 95, 80] 
+#Data setelah diurutkan (Descending): [95, 90, 85, 80, 75, 55, 45] 
+#Tiga nilai tertinggi (Penerima Beasiswa): [95, 90, 85] 
+#Daftar nilai yang lulus: [95, 90, 85, 80, 75]
+# ---------------------------------------------------------
+
+# Tulis kodemu di bawah ini:
